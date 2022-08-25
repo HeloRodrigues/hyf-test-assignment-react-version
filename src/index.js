3c97d8dd1a8458afaad1e59fea1a27f54d08a174
HeloRodrigues <haraurelio@gmail.com>
2022-08-25 14:27:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import App from './App';
 ReactDOM.render (<App />, document.getElementById("root"))
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+
 
 
